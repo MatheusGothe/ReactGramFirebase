d3795df64ae8568e30d97790e356f7342ed75347
MatheusGothe <110669732+MatheusGothe@users.noreply.github.com>
2024-05-07 14:01:41 -0300

    teste

--- src/utils/functions/UploadPhoto.js
+++ src/utils/functions/UploadPhoto.js
@@ -1,17 +1,19 @@
 import { getDownloadURL, getStorage, ref, uploadBytesResumable } from "firebase/storage";
 import { auth, storage } from "../../lib/firebase";
-
+import { v4 as uuidv4 } from "uuid";
 
 // Função para fazer o upload da imagem
-const uploadImage = async({photo:image,photoId}, onProgress) => {
-  const user = auth.currentUser
-   console.log(image)
+const uploadImage = async({photo:image, title: photoId}, onProgress) => {
+
+
   // Verifique o tipo de arquivo
   if (!['image/jpeg', 'image/jpg','image/png','image/avif'].includes(image.type)) {
     throw new Error('Formato de arquivo inválido.');
   }
 
+
   const storageRef = ref(storage, `images/${photoId}`)
+
   const uploadTask = uploadBytesResumable(storageRef,image)
   
   // Crie uma nova Promise para lidar com o processo de upload e obtenção do URL
@@ -20,15 +22,13 @@ const uploadImage = async({photo:image,photoId}, onProgress) => {
       'state_changed',
       snapshot => {
         const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100
-        console.log(progress)
-        console.log(snapshot)
         onProgress(progress); // Chame a função de callback com o progresso atual
         switch (snapshot.state) {
           case 'paused':
-            console.log('Upload is paused');
+          
             break;
           case 'running':
-            console.log('Upload is running');
+            
             break;
         }
       },
