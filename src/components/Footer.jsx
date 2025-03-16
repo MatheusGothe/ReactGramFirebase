8d4cfd18d7c8aec646afef19aab874ac7c1f62e4
MatheusGothe <110669732+MatheusGothe@users.noreply.github.com>
2025-03-16 14:08:07 -0300

    Atualização CopyRight

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import './Footer.css'
 const Footer = () => {
   return (
     <footer id="footer">
-      <p>ReactGram &copy; 2024, <span className="spanC">Matheus Gothe</span> </p>
+      <p>ReactGram &copy; 2025, <span className="spanC">Matheus Gothe</span> </p>
     </footer>
   )
 }
