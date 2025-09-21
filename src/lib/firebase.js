3db0e53e658895a96b43c0dee7992d17d6b08d7e
MatheusGothe <matheusgothe@icloud.com>
2025-09-21 19:22:02 -0300

    Ajustes autenticação do firebase

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -20,7 +20,7 @@ const firebaseConfig = {
 // Initialize Firebase
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp() ;
-const db = getFirestore()
+const db =  getFirestore()
 const auth = getAuth()
 const storage = getStorage()
 
